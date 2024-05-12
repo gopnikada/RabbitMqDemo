c316a6933bbd6201edee1a3cdf8a912fd1d7cf25
gopnikada <33294787+gopnikada@users.noreply.github.com>
2024-05-12 15:31:06 +0200

    Add project files.

---
+++ Receiver1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Receiver1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+            factory.ClientProvidedName = "Rabbit Sender App";
+
+            IConnection connection = factory.CreateConnection();
+
+            IModel channel = connection.CreateModel();
+
+            string exchangeName = "DemoExchange";
+            string routingKey = "demo-routing-key";
+            string queueName = "DemoQueue";
+
+
+            channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+            //durable - save on restart
+            //exclusive - only on one connection + close if connection removed
+            //autodelete - remove if no consumers
+            channel.QueueDeclare(queueName, false, false, false, null);
+            channel.QueueBind(queueName, exchangeName, routingKey, null);
+
+            channel.BasicQos(0, 1, false);
+
+            EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (sender, args) =>
+            {
+                byte[] body = args.Body.ToArray();
+                Console.WriteLine($"Message:" + Encoding.UTF8.GetString(body));
+
+                channel.BasicAck(args.DeliveryTag, false);
+            };
+
+            string consumerTag = channel.BasicConsume(queueName, autoAck: false, consumer);
+            Console.ReadLine();
+
+            channel.BasicCancel(consumerTag);
+
+            connection.Close();
+        }
+    }
+}
---
+++ Sender/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+            factory.ClientProvidedName = "Rabbit Sender App";
+
+            IConnection connection = factory.CreateConnection();
+
+            IModel channel = connection.CreateModel();
+
+            string exchangeName = "DemoExchange";
+            string routingKey = "demo-routing-key";
+            string queueName = "DemoQueue";
+
+
+            channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+            //durable - save on restart
+            //exclusive - only on one connection + close if connection removed
+            //autodelete - remove if no consumers
+            channel.QueueDeclare(queueName, false, false, false, null);
+            channel.QueueBind(queueName, exchangeName, routingKey, null);
+
+            byte[] messageBodyBytes = Encoding.UTF8.GetBytes("Hello world2");
+            channel.BasicPublish(exchangeName, routingKey, null, messageBodyBytes);
+
+            channel.Close();
+            connection.Close();
+        }
+    }
+}
